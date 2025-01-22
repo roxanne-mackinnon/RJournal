79c9fe3273dd782c3a98fff67ba849174f975ef8
roxanne <roxanne_repairs@mailfence.com>
2025-01-21 22:05:08 -0500

    Added new error functionality to AppController with some css to match.

--- src/AppController.js
+++ src/AppController.js
@@ -10,6 +10,7 @@ import {LeftRightView} from './LeftRightView';
 import NoteEditor from './NoteEditor';
 import {NoteController} from './NoteController';
 import LoginPage from './LoginPage';
+import './css/AppController.css';
 
 
 // container for the note list, calendar, and note editor
@@ -17,13 +18,25 @@ export function AppController() {
 
     // Notes that the notelistview should display, to be filtered (either by date or some other means like text search)
     const [filteredNotes, setFilteredNotes] = useState([]);    
+
     // The note that has been clicked for editing/viewing, or null.
     const [activeNote, setActiveNote] = useState(null);
+    // Current date that should be used as the basis for the calendar
     const [date, setDate] = useState(new Date());
-
+    // If user is authenticated or not
     const [authenticated, setAuthenticated] = useState(false);
+    // Current error. After being set to an error state, useEffect will automatically clear it
+    // after a short timeout
+    const [error, setError] = useState(null);
 
     const isEditing = (activeNote !== null);
+
+    useEffect(() => {
+        if (error === null) return;
+
+        setTimeout(() => setError(null), 5000);
+    }, [error]);
+
 
     const onCalendarRangeSelected = (start, end) => {
         setFilteredNotes(NoteController.findBetweenDates(start, end));
@@ -38,10 +51,17 @@ export function AppController() {
     const leftView = <NoteListView notes={filteredNotes} onNoteSelected={(note) => setActiveNote(note)}/>;
     const rightView = isEditing
                         ? <NoteEditor note={activeNote} setNote={setActiveNote} onSubmit={onNoteEditSubmit} onCancel={() => setActiveNote(null)}/>
-                        : <Calendar date={date} setDate={setDate} onRangeSelected={onCalendarRangeSelected} />;
+                        : <>
+                            <Calendar date={date} setDate={setDate} onRangeSelected={onCalendarRangeSelected} />
+                            <button onClick={() => setError(new Error("Test error message."))}>Set Error</button>
+                          </>
 
 
-    return (authenticated ? <LeftRightView left={leftView} right={rightView} />
-                          : <LoginPage setAuthenticated={setAuthenticated} />
+    return (
+    <>
+        {authenticated ? <LeftRightView left={leftView} right={rightView} />
+                       : <LoginPage setAuthenticated={setAuthenticated} />}
+        {error && <p id="error-toast">{error.name}: {error.message}</p>}
+    </>
     );
 }
